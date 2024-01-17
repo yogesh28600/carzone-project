855d20fa4e6833ea21e09bd3b7cc97120b39d781
Yogesh kumar Nandi <yogesh28600@gmail.com>
2024-01-17 21:29:27 +0530

    Added Team Model class

--- carzone/pages/admin.py
+++ carzone/pages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
+from django.contrib.auth.models import Group
 
 # Register your models here.
+class TeamAdmin(admin.ModelAdmin):
+    list_display=('id','first_name','designation')
+    list_display_links=('id','first_name')
+
+admin.site.register(models.Team,TeamAdmin)
+admin.site.unregister(Group)
