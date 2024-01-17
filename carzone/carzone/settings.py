f5b6c012fd01afa4e3df68411379357b24f54b1a
Yogesh kumar Nandi <yogesh28600@gmail.com>
2024-01-17 23:51:10 +0530

    Configured Media Settings

--- carzone/carzone/settings.py
+++ carzone/carzone/settings.py
@@ -126,3 +126,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media settings
+import os
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
