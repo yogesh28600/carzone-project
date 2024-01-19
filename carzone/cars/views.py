765b9f10a4f83f6c327c1b3309f958940f92ac8b
Yogesh kumar Nandi <yogesh28600@gmail.com>
2024-01-19 14:51:12 +0530

    Added Pagination for cars

--- carzone/cars/views.py
+++ carzone/cars/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Car
+from django.core.paginator import Paginator
 # Create your views here.
 def cars_home(request):
     cars = Car.objects.all()
+    paginator = Paginator(cars,4)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
     data = {
-        'cars':cars
+        'cars':paged_cars
     }
     return render(request,'cars/cars_home.html',data)
